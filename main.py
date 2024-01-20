31ce898740ce4a73dd787c7374764fbc6efe2b3d
Santielpapi <msjimenez@unbosque.edu.co>
2024-01-20 14:12:35 -0500

    1.1

---
+++ main.py
@@ -0,0 +1,5 @@
+from utils import preprocessing
+
+data = preprocessing().creation_data('Salary_Data.csv')
+target, features = preprocessing().declaration_var(data, 'Salary')
+print(target)
--- utils.py
+++ utils.py
@@ -1,6 +1,22 @@
 import pandas as pd 
 
-df = pd.read_csv('Salary_Data.csv')
+class preprocessing:
+    def creation_data(self, path_csv):
+        df = pd.read_csv(path_csv)
+        return df
+    
+    def declaration_var(self, data, target):
+        target_1 = data[target]
+        features = data.drop(target, axis=1)
+        return target_1, features
+    
+    
+    
+
+data = preprocessing().creation_data('Salary_Data.csv')
+target, features = preprocessing().declaration_var(data, 'Salary')
 
 
-print(df)
+print(target, features)
+
+# print(data.info())
