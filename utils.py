1a9489ac4290ea63b9575756509f720df36c23d9
Santielpapi <msjimenez@unbosque.edu.co>
2024-01-20 17:26:30 -0500

    1.4

--- utils.py
+++ utils.py
@@ -9,9 +9,17 @@ class preprocessing:
     def declaration_var(self, data, target):
         target_1 = data[target]
         features = data.drop(target, axis=1)
-        
+        names_f = {name:set() for name in features}
+        for i in features:
+            for j in features[i]:
+                names_f[i].add(j)
+        for i in features:
+            index_drop = features[i][features[i].isnull() == True].index
+            features.drop(index_drop, inplace = True)
         
         return target_1, features
+    
+
     
     
 data = preprocessing().creation_data('Salary_Data.csv')
@@ -19,8 +27,10 @@ target, features = preprocessing().declaration_var(data, 'Salary')
 # print(data.info())
 
 
-names = {name for name in features}
-print(names)
 
 
+
+
+print(features.isnull().sum())
+print(len(features['Age']))
 # print(data.info())
